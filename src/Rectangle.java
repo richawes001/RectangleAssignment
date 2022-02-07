9f0a4316f6bd3b759a476227ef24d7e19aa867d1
richawes001 <richawes001@warren.k12.in.us>
2022-02-07 11:54:31 -0500

    Initial commit

---
+++ src/Rectangle.java
@@ -0,0 +1,30 @@
+public class Rectangle {
+    protected double length;
+    protected double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getLength(){
+        return this.length;
+    }
+
+    public double getWidth(){
+        return this.width;
+    }
+
+    public void setLength(double length){
+        this.length = length;
+    }
+
+    public void setWidth(double width){
+        this.width = width;
+    }
+
+    public double Area(){
+        return this.width * this.length;
+    }
+
+}
---
+++ src/Square.java
@@ -0,0 +1,17 @@
+public final class Square extends Rectangle{
+
+    public Square(double sideLength) {
+        super(sideLength, sideLength);
+
+    }
+
+    public double getSideLength(){
+        return this.length;
+    }
+
+    public void setSideLength(double width){
+        this.length = width;
+        this.width = width;
+    }
+
+}
